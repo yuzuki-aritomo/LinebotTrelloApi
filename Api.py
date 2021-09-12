fcbceb7af33cae89291f7613be63a7f52446e57b
yuzuki <yuzuki.aritomo@gmail.com>
2021-09-13 00:33:28 +0900

    add: card新規作成

--- Api.py
+++ Api.py
@@ -14,19 +14,22 @@ card_name = "test add"
 query = {
   'key': '4a622c5834edd8afcb884ffd870cbcd8',
   'token': 'bd4be36f22d5220491c82668dff1b3084e6e3a87495566841c536bc29e85b4a3',
-  'idList': '5f329cbc131beb79fc75c28e',
-  'name': card_name
 }
 
-response = requests.request(
-  "POST",
-  url,
-  headers=headers,
-  params=query
-)
-print(response)
-# print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
-
-#ログに出力
-with open('log.json', mode='wt', encoding='utf-8') as file:
-  json.dump(json.loads(response.text), file, sort_keys=True, indent=4, separators=(",", ": "))
+def createCard(ReqText):
+  query['name'] = ReqText
+  query['idList'] = '5f329cbc131beb79fc75c28e'
+  response = ""
+  try:
+    response = requests.request(
+      "POST",
+      url,
+      headers=headers,
+      params=query,
+    )
+    return True
+  except:
+    #ログに出力
+    with open('log.json', mode='wt', encoding='utf-8') as file:
+      json.dump(json.loads(response.text), file, sort_keys=True, indent=4, separators=(",", ": "))
+    return False
--- App.py
+++ App.py
@@ -6,16 +6,19 @@ from linebot.exceptions import (
   InvalidSignatureError
 )
 from linebot.models import (
-  MessageEvent, TextMessage, TextSendMessage,
+  MessageEvent, TextMessage, TextSendMessage, messages,
 )
 import os
-
-
+from Api import createCard
+import json
 app = Flask(__name__)
 
+YOUR_CHANNEL_ACCESS_TOKEN = 'BgL5Q+YnHFN+VtkGDYKrTSYyoDpkUVcPmUlG6bWCYmnESi4lgNYnHjTd2qzeOqrpsjJOKi8QZaSQa/LQcf4Cxd8wcQua8LOEr6fojr8ceIm3Y6naGj3331BnbpFmQwGsooT7xiHfNWVovEVJEkDRBgdB04t89/1O/w1cDnyilFU='
+YOUR_CHANNEL_SECRET = '5521bb2a895206ab596a0334730d12a6'
+
 #環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
-YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+# YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+# YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
@@ -34,20 +37,26 @@ def callback():
   body = request.get_data(as_text=True)
   app.logger.info("Request body: " + body)
 
+  # ReqContent = request.get_json()
+  # ReqText = ReqContent['events'][0]['message']['text']
   # handle webhook body
   try:
+    print("api呼び出し")
     handler.handle(body, signature)
   except InvalidSignatureError:
     abort(400)
-
   return 'OK'
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+  if createCard(event.message.text):
+    ReplyText = "カードを作成しました。"
+  else:
+    ReplyText = "カード作成に失敗しました。"
   line_bot_api.reply_message(
     event.reply_token,
-    TextSendMessage(text=event.message.text))
+    TextSendMessage(text=ReplyText))
 
 
 if __name__ == "__main__":
